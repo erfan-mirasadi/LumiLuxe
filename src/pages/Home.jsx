219cc8b129f1c96fa466ef8d48ae3726ea696a0b
erfan-mirasadi <erfan.mirasadi@gmail.com>
2025-08-25 22:13:20 +0300

    some bugs fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import About from "../components/About";
 import Hero from "../components/Hero";
 import Features from "../components/Features";
 import Story from "../components/Story";
-import Contact from "../components/contact";
+import Contact from "../components/Contact";
 
 function Home() {
   return (
